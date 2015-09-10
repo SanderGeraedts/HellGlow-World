0e1304b548f65c58549f289be38c9c0dff7cc5ba
Fhict <fhict@MacBook-Pro.local>
2015-09-10 10:40:01 +0200

    Initial Commit

---
+++ HellGlow World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HellGlow World
+//
+//  Created by Fhict on 10/09/15.
+//  Copyright (c) 2015 Codepanda. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
